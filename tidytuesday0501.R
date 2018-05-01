0fa1818fcf2b0d67abccced1af051dc0a72aa665
cteitel <claire.teitelbaum@gmail.com>
2018-05-01 18:01:16 -0400

    may 5

---
+++ tidytuesday0501.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(ggplot2)
+
+acs = read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/acs2015_county_data.csv",
+               stringsAsFactors = F)
+head(acs)
+
+acs = arrange(acs , desc(Income))
+acs_states = group_by(acs , State)
+
+is.numeric_int = function(x) { is.numeric(x) | is.integer(x) }
+acs_max = summarize_if(acs_states , is.numeric_int , c("first" , "last" , "mean"))
+
+acs_max = arrange(acs_max , Income_first)
+
+p <- ggplot(acs_max) +
+  geom_segment(aes(x = Income_first , xend = 0 , y = 1:nrow(acs_max) , yend = 1:nrow(acs_max),
+                   color = Black_first) , size = 2) +
+  geom_segment(aes(x = (-1) * IncomePerCap_last , xend = 0 , y = 1:nrow(acs_max) , yend = 1:nrow(acs_max),
+               color =  Black_last), size = 2) +
+  geom_point(aes(x = Income_first ,  y = 1:nrow(acs_max))) +
+  geom_point(aes(x = (-1) * IncomePerCap_last ,  y = 1:nrow(acs_max) )) +
+  scale_colour_gradient2(low = "blue",mid="grey",high = "red" ,name="Percent black",midpoint=12.3) +
+  theme_classic() + 
+  theme(axis.ticks.y = element_blank() , axis.text.y = element_blank() , axis.title.y = element_blank()) +
+  ggtitle("Income of highest- and lowest- earning counties") +
+  scale_x_continuous("Household Income (K USD)", breaks = c(-50000,0,50000,100000) , labels = c(50,0,50,100)) +
+  geom_vline(xintercept = 0)+
+  # geom_text(aes(x = -1 * Income_last , y = seq(1,nrow(acs_max),2) , label = State) , 
+  #           data = slice(acs_max,seq(1,nrow(acs_max),2)) , size= 3 , hjust = 1) + 
+  # geom_text(aes(x = Income_first , y = seq(2,nrow(acs_max),2) , label = State)  , 
+  #           data = slice(acs_max,seq(2,nrow(acs_max),2)) ,  hjust = 0 , size = 3) +
+  geom_text(aes(x = Income_first+3000 , y = seq(1,nrow(acs_max),1) , label = State) , 
+            size= 3 ,hjust = 0) +
+  geom_text(aes(x = -3000 , y = 54 , label = c("Lowest earning")) , hjust = 1) + 
+  geom_text(aes(x = 3000 , y = 54 , label = c("Highest earning")) , hjust = 0)  
+  p
+
+
