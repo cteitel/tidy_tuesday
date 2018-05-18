0ffe6fc2f870e90d3b71c0861fa29c3afb00507f
cteitel <claire.teitelbaum@gmail.com>
2018-05-18 09:34:38 -0400

    final commit

--- tidytuesday0515.R
+++ tidytuesday0515.R
@@ -4,19 +4,31 @@ sw <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/st
 #csv did not read with header b/c of question marks
 #get questions and remove from dataset
 questions <- as.character(sw[1,])
+movies = as.character(sw[2,4:9])
 sw = sw[-c(1:2),]
 
-#separate out multiple-choice questions
+#separate out multiple-choice questions: movies seen
 filmsSeen <- select(sw , c(1,4:9))
 colnames(filmsSeen)[c(2:7)] <- apply(filmsSeen[,c(2:7)] , 2 , function(x) unique(x[x!=""]))
 filmsSeen <- mutate_at(filmsSeen , c(2:7) , function(x) x!="")
+#NOT used here
 
-filmsSeen = full_join(filmsSeen , select(sw , c(1,34:38)) , by = "V1")
-colnames(filmsSeen)[c(8:12)] <- questions[c(34:38)]
+#separate out multiple-choice questions: favorite movies
+favorites <- select(sw , c(1,11,34:38))
+colnames(favorites) <- c("ID","favorite",questions[c(34:38)])
+favorites <- filter(favorites , favorite != "" & Age!="")
+favorites <- mutate(favorites , favorite = as.numeric(favorite))
+favorites <- mutate(favorites , faveName = movies[favorites$favorite])
+favorites$Age = gsub("> 60" , "60+" , favorites$Age)
 
-filmsByAge = gather(select(filmsSeen , c(2:7,9)) , Movie , Seen , 1:6)
-filmsByAge = group_by(filmsByAge , Age)
-filmsByAge = summarize(filmsByAge , Movie)
 
-plot <- geom_bar(aes(x = Age , y = Seen) , filmsByAge)
+plot <- ggplot(aes(x = Age) , data = favorites) + 
+  geom_bar(aes(fill = faveName) , position = "fill") + 
+  theme_classic() +
+  scale_fill_brewer("Favorite movie",palette = "Set2") +
+  ylab("Proportion") +
+  ggtitle("Everyone Likes Episode V") +
+  theme(axis.text=element_text(size=11) , 
+        axis.title=element_text(size=14),
+        title=element_text(size=16))
 plot
