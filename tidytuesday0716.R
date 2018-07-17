0e95a625377c0760c37e24f0bc9a9f090d781554
cteitel <claire.teitelbaum@gmail.com>
2018-07-17 10:59:52 -0400

    final code and plot

--- tidytuesday0716.R
+++ tidytuesday0716.R
@@ -26,6 +26,10 @@ exercise <- left_join(exercise , acs_states , by = c("state"="State")) %>%
 exercise <- mutate(exercise , stateNum = match(state , state.name)) %>%
   mutate(st = state.abb[stateNum] , div = state.division[stateNum])
 
+#group into fewer regions (remove "east" and "west" subdivisions)
+exercise <- mutate(exercise , divNew = div) %>%
+  mutate(divNew = str_replace(divNew , c("East ") , "")) %>%
+  mutate(divNew = str_replace(divNew , c("West ") , ""))
 
 plotdats = filter(exercise , !is.na(Income))
 
@@ -33,10 +37,19 @@ plotdats = filter(exercise , !is.na(Income))
 #plot
 p <- ggplot(filter(plotdats , state != "District of Columbia") , 
             aes(x = Walk , y = adults)) +
-  geom_point(aes(color = div) , size = 3) +
+  geom_point(aes(color = divNew) , size = 3 ) +
   theme_classic() +
-  #scale_color_continuous("Per capita \nincome", low = "red" , high = "black") +
-  geom_text(aes(label = st , color = div) , hjust = -.5) +
-  NULL
+  scale_color_brewer("Region" , palette = "Set2") +
+  geom_text(aes(label = st , color = divNew) , fontface = "bold" ,
+            position = position_nudge(x = -0.2 , y = -0.2) ,
+              check_overlap = T) +
+  ylab("Adults meeting exercise guidelines (%)") + 
+  xlab("Adults walking to work (%)") + 
+  theme(axis.title = element_text(size=14),
+        axis.text = element_text(size=14),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(face = "bold"),
+        legend.position = "bottom")+
+  NULL 
 p
 
