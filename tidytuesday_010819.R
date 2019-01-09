5e7104e7f2ab5842426b5a9c9e38262b7ecf16e5
Claire <claire.teitelbaum@gmail.com>
2019-01-09 17:52:41 -0500

    Update tidytuesday_010819.R

--- tidytuesday_010819.R
+++ tidytuesday_010819.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 ratings_in <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-01-08/IMDb_Economist_tv_ratings.csv")
 ratings_in <- rownames_to_column(ratings_in)
-  
+
 allGenreNames <- ratings_in$genres %>%
   paste(collapse = ",") %>%
   str_split(",") %>%
@@ -28,8 +28,8 @@ mod2 <- lm(share ~ . , data = select(ratings , allGenreNames , seasonNumber , sh
 summary(mod2)
 
 
-ggplot(ratings , aes(x = seasonNumber , y = share)) +
-  geom_point() +
+ggplot(ratings , aes(x = date , y = av_rating)) +
+  geom_point(aes(size = share)) +
   geom_line(aes(group = titleId)) +
   theme_classic()
 
@@ -39,7 +39,38 @@ Ns = group_by(ratings_long , genres_all) %>%
   summarize(N = n()) %>%
   arrange(desc(N))
 ratings_long <- mutate(ratings_long , genres_all = factor(ratings_long$genres_all , levels = Ns$genres_all))
-include = filter(Ns , N > 200)$genres_all
+include = filter(Ns , N > 200 & genres_all != "Drama")$genres_all
+
+
+
+meanRating = mean(ratings$av_rating)
+meanShare = mean(ratings$share)
+
+ratings = mutate(ratings , 
+                 status = ifelse(av_rating > meanRating & share < meanShare , "underwatched",
+                                 ifelse(av_rating < meanRating & share > meanShare , "overwatched",
+                                        "expected")))
+ratings_long = mutate(ratings_long , 
+                 status = ifelse(av_rating > meanRating & share < meanShare , "underwatched",
+                                 ifelse(av_rating < meanRating & share > meanShare , "overwatched",
+                                        "expected")))
+
+ggplot(filter(ratings , share > 0) , 
+       aes(y = share , x = av_rating)) +
+  geom_point(aes(color= status)) +
+  geom_vline(aes(xintercept = meanRating)) +
+  geom_hline(aes(yintercept = meanShare)) +
+  scale_y_continuous(trans = "log" , breaks = c(0.05 , 1 , 20) , labels = paste(c(0.05 , 1 , 20) , "%")) +
+  theme_classic() +
+  scale_color_brewer(palette = "Set2" , name = "Number watching")
+
+ggplot(filter(ratings_long, share > 0)) +
+  geom_bar(aes(x = status , fill = status)) +
+  scale_color_brewer(palette = "Set2" , name = "Number watching") +
+  facet_wrap(~genres_all)
+
+
+
 
 ggplot(filter(ratings_long , genres_all %in% include & seasonNumber <=15) , 
        aes(x = date , y = share)) +
@@ -47,3 +78,12 @@ ggplot(filter(ratings_long , genres_all %in% include & seasonNumber <=15) ,
   geom_line(aes(group = titleId)) +
   theme_classic() +
   facet_wrap(~genres_all)
+
+
+
+ggplot(filter(ratings) , 
+       aes(y = share , x = av_rating)) +
+  geom_point(aes(color= Action , fill = Action)) +
+  geom_vline(aes(xintercept = mean(ratings$av_rating)))
+
+
